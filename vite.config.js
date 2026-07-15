b5a12663d1150043b823c301dd8ba0c98dd9e498
ArunNrwade <arunnarwade005@gmail.com>
2026-07-15 09:51:16 +0530

    Fix Vite base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/FinanceTracker/"   // ⭐ MOST IMPORTANT LINE
-})
+});
+
+
+
+
+// git add .
+// git commit -m "Fix Vite base path for deployment"
+// git branch -M main
+// git push -u origin main
